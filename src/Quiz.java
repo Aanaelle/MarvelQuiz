f1358e5ceaa47e40891ba62217a7c7c65763ac82
Aanaelle <anaellemimi27@gmail.com>
2022-07-01 16:13:35 +0200

    creation textfield et textarea

--- src/Quiz.java
+++ src/Quiz.java
@@ -56,8 +56,36 @@ public class Quiz implements ActionListener
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setSize(650,650);
         this.frame.getContentPane().setBackground(new Color(50,50,50));
+        this.frame.setLayout(null);
+        this.frame.setResizable(false);
+
+        
+        //création textfield
+        this.txtfield.setBounds(0,0,650,50);
+        this.txtfield.setBackground(new Color(25,25,25));
+        this.txtfield.setForeground(new Color(150,200,200));
+        this.txtfield.setFont(new Font("Ink Free", Font.PLAIN, 30));
+        this.txtfield.setBorder(BorderFactory.createBevelBorder(1));
+        this.txtfield.setHorizontalAlignment(JTextField.CENTER);
+        this.txtfield.setEditable(false);
+
+        //création textArea
+        this.txtArea.setBounds(0,50,650,50);
+        this.txtArea.setLineWrap(true);
+        this.txtArea.setWrapStyleWord(true);
+        this.txtArea.setBackground(new Color(25,25,25));
+        this.txtArea.setForeground(new Color(150,200,200));
+        this.txtArea.setFont(new Font("MV Boli", Font.BOLD, 25));
+        this.txtArea.setBorder(BorderFactory.createBevelBorder(1));
+        this.txtArea.setEditable(false);
 
 
+        //création des boutons
+        
+        
+
+        this.frame.add(this.txtArea);
+        this.frame.add(this.txtfield);
         this.frame.setVisible(true);
     }
 
